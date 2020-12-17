f32544668c36180d0305e2239ce7376024bd76dc
Kaleb McKone <kmckone1825@gmail.com>
2020-12-17 08:11:18 -0600

    Restructure repo. Closes #5

---
+++ cmd/lk-site/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/krmckone/ksite/internal/config"
+	"github.com/krmckone/ksite/internal/preprocessor"
+	"github.com/krmckone/ksite/internal/utils"
+)
+
+func main() {
+	utils.Clean("build")
+	config := config.ReadConfig("configs/config.yml")
+	md := utils.ReadFile("assets/index.md")
+	md = preprocessor.Run(md, config.Template.Params)
+	htmlOpts := html.RendererOptions{
+		CSS:   config.Template.Styles.SheetURL,
+		Flags: html.CommonFlags | html.CompletePage,
+	}
+	renderer := html.NewRenderer(htmlOpts)
+	output := markdown.ToHTML(md, nil, renderer)
+	utils.Mkdir("build")
+	utils.WriteFile("build/index.html", output)
+}
---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+
+	"github.com/krmckone/ksite/internal/utils"
+	"gopkg.in/yaml.v2"
+)
+
+// Config top level project config settings
+type Config struct {
+	Template TemplateConfig `yaml:"template"`
+}
+
+// TemplateConfig config for the html templating
+type TemplateConfig struct {
+	Params Params       `yaml:"params"`
+	Styles StylesParams `yaml:"styles"`
+}
+
+// Params template variable parameters config
+type Params map[string]string
+
+// StylesParams parameters for stylesheets
+type StylesParams struct {
+	SheetURL string `yaml:"sheetURL"`
+}
+
+// ReadConfig reads in the project config yaml located at path
+func ReadConfig(path string) Config {
+	b := utils.ReadFile(path)
+	config := Config{}
+	err := yaml.Unmarshal(b, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
