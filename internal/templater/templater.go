0a53147a2ba379f3272dee34021930da398433ed
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2024-04-16 18:54:45 -0400

    Fix calls

--- internal/templater/templater.go
+++ internal/templater/templater.go
@@ -16,6 +16,7 @@ import (
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
 	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 // Page holds data for templating a page
@@ -264,5 +265,8 @@ func newPage(title string, content []byte, template []byte, params map[string]st
 
 func makeNavTitleFromHref(assetHref string) string {
 	pathSplit := strings.Split(assetHref, "/")
-	return cases.Title(strings.Join(strings.Split(pathSplit[len(pathSplit)-1], "_"), " "))
+	caser := cases.Title(language.AmericanEnglish)
+	return caser.String(
+		strings.Join(strings.Split(pathSplit[len(pathSplit)-1], "_"), " "),
+	)
 }
