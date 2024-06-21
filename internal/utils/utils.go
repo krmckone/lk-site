ea43c51d5210e3f36965498c6a5c7150af4741db
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2024-06-21 15:36:46 -0400

    Update error messaging

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -9,11 +9,11 @@ import (
 	"time"
 )
 
-// ReadFile wrapper for ioutil.ReadFile
+// ReadFile wrapper for os.ReadFile
 func ReadFile(path string) []byte {
 	bytes, err := os.ReadFile(path)
 	if err != nil {
-		return []byte{}
+    log.Fatalf("Error reading file at %s: %s", path, err)
 	}
 	return bytes
 }
@@ -21,21 +21,21 @@ func ReadFile(path string) []byte {
 // WriteFile wrapper for ioutil.WriteFile
 func WriteFile(path string, b []byte) {
 	if err := os.WriteFile(path, b, 0644); err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error writing file at %s: %s", path, err)
 	}
 }
 
 // Mkdir wrapper for os.MkdirAll
 func Mkdir(path string) {
 	if err := os.MkdirAll(path, 0755); err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error making directory at %s: %s", path, err)
 	}
 }
 
 // Clean cleans the directory at path
 func Clean(path string) {
 	if err := os.RemoveAll(path); err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error cleaning path %s: %s", path, err)
 	}
 }
 
@@ -43,7 +43,7 @@ func Clean(path string) {
 func GetCurrentEasternTime() string {
 	location, err := time.LoadLocation("America/New_York")
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error getting the current EST time: %s", err)
 	}
 	return time.Now().In(location).Format(time.RFC822)
 }
@@ -57,7 +57,7 @@ func GetCurrentYear() string {
 func CopyFiles(srcPath, dstPath string) {
 	entries, err := os.ReadDir(srcPath)
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error reading directory at %s: %s", srcPath, err)
 	}
 	for _, entry := range entries {
 		if !entry.Type().IsDir() {
@@ -78,7 +78,7 @@ func CopyFiles(srcPath, dstPath string) {
 func copyFile(srcPath, dstPath string) {
 	sourceFileStat, err := os.Stat(srcPath)
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error getting description for file at %s: %s", srcPath, err)
 	}
 
 	if !sourceFileStat.Mode().IsRegular() {
@@ -87,17 +87,17 @@ func copyFile(srcPath, dstPath string) {
 
 	source, err := os.Open(srcPath)
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error opening file at %s: %s", srcPath, err)
 	}
 	defer source.Close()
 
 	destination, err := os.Create(dstPath)
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error creating file at %s: %s", dstPath, err)
 	}
 	defer destination.Close()
 	_, err = io.Copy(destination, source)
 	if err != nil {
-		log.Fatal(err)
+    log.Fatalf("Error copying file from %s to %s: %s", srcPath, dstPath, err)
 	}
 }
