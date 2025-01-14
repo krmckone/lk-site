2edc0bd4d3e623cf42fec1b319ed1503717966a1
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2025-01-14 14:52:36 -0600

    Cleanup

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Function to configure Git
+RELEASE_DATE=$(date +%m-%d-%y-%H:%M:%S)
+REFERENCE_LINK="krmckone/lk-site@$(git rev-parse --short $GITHUB_SHA)"
+
 configure_git() {
   git config --global user.name "Kaleb's GitHub Actions Bot from lk-site"
   git config --global user.email "20476319+krmckone@users.noreply.github.com" # TODO: Parameterize
 }
 
-# Function to create a new branch for deployment
 create_deployment_branch() {
   local release_hash=$(git rev-parse --short $GITHUB_SHA)
   git checkout -b "lk-site-deploy-$GITHUB_REF_NAME-$release_hash"
 }
 
-# Function to copy site files
 copy_site_files() {
   cp -r ~/site/* ./
 }
 
-# Function to check for changes and commit them
 commit_changes() {
+  git status
   local site_modified=$(git status --porcelain)
   if [[ -z $site_modified ]]; then
     echo "No changes to the static site. Exiting without asset deployment."
@@ -26,23 +26,19 @@ commit_changes() {
   else
     echo "Deploying changes to the static site."
     git add .
-    git commit -m "New Release $reference_link $release_date"
-    git push origin HEAD
+    git commit -m "New Release $REFERENCE_LINK $RELEASE_DATE"
+    git push origin
   fi
 }
 
-# Function to create and merge a pull request
 create_and_merge_pr() {
-  local release_date=$(date +%m-%d-%y-%H:%M:%S)
-  local reference_link="krmckone/lk-site@$(git rev-parse --short $GITHUB_SHA)"
+  echo -e "* This pull request was automatically created and merged by $GITHUB_SERVER_URL/krmckone/lk-site/actions/runs/$GITHUB_RUN_ID.\n* This release is based on $REFERENCE_LINK" > body
+  gh pr create --title "Automatic Release $RELEASE_DATE" --body-file body
 
-  echo -e "* This pull request was automatically created and merged by $GITHUB_SERVER_URL/krmckone/lk-site/actions/runs/$GITHUB_RUN_ID.\n* This release is based on $reference_link" > body
-  gh pr create --title "Automatic Release $release_date" --body-file body
-
-  local max_retries=4
+  local max_retries=2
   local retries=0
   until gh pr merge --auto --merge; do
-    sleep 5
+    sleep 3
     [[ $retries -eq $max_retries ]] && echo 'Unable to merge PR in krm-site!' && exit 1
     ((retries++))
     echo "GH CLI returned non-zero after $retries attempts, going to retry auto merge"
@@ -52,7 +48,6 @@ create_and_merge_pr() {
 
 # Main script execution
 git --version
-git pull origin main
 configure_git
 create_deployment_branch
 copy_site_files
